6d9c1d9f3d95cc0b0d2c886b3d6838df5d9927f1
Ales SITS <ales.bachtik@sits.msf.org>
2023-04-03 12:45:08 +0200

    Working version with setInterval workaround

--- src/extensions/msfListDescription/MsfListDescriptionCommandSet.ts
+++ src/extensions/msfListDescription/MsfListDescriptionCommandSet.ts
@@ -1,6 +1,5 @@
 import {
-  BaseListViewCommandSet,
-  ListViewStateChangedEventArgs
+  BaseListViewCommandSet
 } from '@microsoft/sp-listview-extensibility';
 import { spfi, SPFx } from "@pnp/sp";
 import "@pnp/sp/webs";
@@ -10,22 +9,17 @@ import "@pnp/sp/items";
 
 
 export interface IMsfListDescriptionCommandSetProperties {
-
-  sampleTextOne: string;
-  sampleTextTwo: string;
 }
-
 
 export default class MsfListDescriptionCommandSet extends BaseListViewCommandSet<IMsfListDescriptionCommandSetProperties> {
   public onInit(): Promise<void> {
-    console.log("Init run")
-
+    
+    let pageUrl = this.context.pageContext.list.serverRelativeUrl
     const libhead = document.querySelector(".od-ItemsScopeItemContent-header")
     const sp = spfi().using(SPFx(this.context));
     const listTitle: string =  `${this.context.pageContext.list.title}`
   
     sp.web.lists.getByTitle(listTitle).select("Description")().then((result) => {
-      console.log("rendering...")
       const old = document.getElementById("spfx_des")
       old === null? "" : old.remove()
       const description:string = result.Description;
@@ -45,53 +39,47 @@ export default class MsfListDescriptionCommandSet extends BaseListViewCommandSet
       </div> 
       `) 
       : libhead.insertAdjacentHTML('beforeend',``);
-    
-      console.log("rendering finished...");
     }).catch((error) => {
       console.log(error);
     });
 
-    this.context.listView.listViewStateChangedEvent.add(this, this.onListViewUpdatedv2);
-    //this.context.listView.listViewStateChangedEvent.add(this, this._onListViewStateChanged);
+    setInterval(()=>{
+       if(pageUrl!==this.context.pageContext.list.serverRelativeUrl) {  
+
+
+    const libhead = document.querySelector(".od-ItemsScopeItemContent-header")
+    const sp = spfi().using(SPFx(this.context));
+    const listTitle: string =  `${this.context.pageContext.list.title}`
+  
+    sp.web.lists.getByTitle(listTitle).select("Description")().then((result) => {
+     
+      const old = document.getElementById("spfx_des")
+      old === null? "" : old.remove()
+      const description:string = result.Description;
+      description.includes('<script') ? "" :
+      description.startsWith('description=true:') ? 
+      libhead.insertAdjacentHTML('afterend',`
+      <div id="spfx_des" style="
+      min-height: 45px;
+      width: 100%;
+      margin: 0 10px;
+      padding: 0 20px;
+      overflow: hidden;
+      position: sticky;
+      z-index: 1;
+      top:5px
+      ">${description.replace('description=true:','')}
+      </div> 
+      `) 
+      : libhead.insertAdjacentHTML('beforeend',``);
+    }).catch((error) => {
+      console.log(error);
+    });
+    
+      }
+  },4000)
 
     return Promise.resolve();
   }
-
-  public onListViewUpdatedv2(args: ListViewStateChangedEventArgs): void {
-    console.log("Update run")
-    document.getElementById("spfx_des").remove()
-    const libhead = document.querySelector(".od-ItemsScopeItemContent-header")
-    const sp = spfi().using(SPFx(this.context));
-    const listTitle: string =  `${this.context.pageContext.list.title}`
-  
-    sp.web.lists.getByTitle(listTitle).select("Description")().then((result) => {
-      console.log("rendering updated...")
-      const old = document.getElementById("spfx_des")
-      old === null? "" : old.remove()
-      const description:string = result.Description;
-      description.includes('<script') ? "" :
-      description.startsWith('description=true:') ? 
-      libhead.insertAdjacentHTML('afterend',`
-      <div id="spfx_des" style="
-      min-height: 45px;
-      width: 100%;
-      margin: 0 10px;
-      padding: 0 20px;
-      overflow: hidden;
-      position: sticky;
-      z-index: 1;
-      top:5px
-      ">${description.replace('description=true:','')}
-      </div> 
-      `) 
-      : libhead.insertAdjacentHTML('beforeend',``);
-    
-      //console.log(description);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-
-
 
 }
